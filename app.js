d172e599a02061c23dcbbd9ae8300d8df6e32440
Nikhil Singh <Singh.nikhil999@gmail.com>
2017-12-28 18:39:44 +0530

    final routes

--- app.js
+++ app.js
@@ -1,62 +1,99 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/restful_blog_app", {useMongoClient: true});
-	
-// app config
+var express = require("express"),
+    app     = express(),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    expressSanitizer = require("express-sanitizer"),
+    methodOverride = require('method-override');
+    
+mongoose.connect("mongodb://localhost/blog_app");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
-
-// Mongoose config
+app.use(methodOverride('_method'));
 
 var blogSchema = new mongoose.Schema({
-	title: String,
-	image: String,
-	body : String,
-	create : {type: Date, default: Date.now}
+    title: String,
+    body: String,
+    image: String,
+    created:  {type: Date, default: Date.now}
 });
 
-var Blog = mongoose.model("Blog",blogSchema);
+var Blog = mongoose.model("Blog", blogSchema);
 
-
-
-// RESTful Routes
-
-app.get("/",function(req,res){
-	res.redirect("/blogs");
+app.get("/", function(req, res){
+    res.redirect("/blogs");
 });
 
-app.get("/blogs",function(req,res){
-	Blog.find({},function(err,blogs){
-		if(err){
-			console.log(err);
-		}else{
-			res.render("index",{blogs: blogs});
-		}
-	})
-	
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {blogs: []}); 
+        }
+    })
 });
 
-app.get("/blogs/new",function(req,res){
-	res.render("new");
+app.get("/blogs/new", function(req, res){
+   res.render("new"); 
 });
 
-app.post("/blogs",function(req,res){
-	Blog.create(req.body.blog,function(err,newBlog){
-		if(err){
-			res.render("new");
-		}else{
-			res.redirect("/blogs");
-		}
-	})
+app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+   var formData = req.body.blog;
+   Blog.create(formData, function(err, newBlog){
+       console.log(newBlog);
+      if(err){
+          res.render("new");
+      } else {
+          res.redirect("/blogs");
+      }
+   });
 });
 
-app.get("/blogs/:id",function(req,res){
-	res.send("Blog page");
+app.get("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, blog){
+      if(err){
+          res.redirect("/");
+      } else {
+          res.render("show", {blog: blog});
+      }
+   });
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+   Blog.findById(req.params.id, function(err, blog){
+       if(err){
+           console.log(err);
+           res.redirect("/")
+       } else {
+           res.render("edit", {blog: blog});
+       }
+   });
+});
+
+app.put("/blogs/:id", function(req, res){
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, blog){
+       if(err){
+           console.log(err);
+       } else {
+         var showUrl = "/blogs/" + blog._id;
+         res.redirect(showUrl);
+       }
+   });
+});
+
+app.delete("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, blog){
+       if(err){
+           console.log(err);
+       } else {
+           blog.remove();
+           res.redirect("/blogs");
+       }
+   }); 
 });
 
 
-app.listen(3000);
+app.listen(process.env.PORT, process.env.IP);
